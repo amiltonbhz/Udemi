50955873481a6960c63ca4b34a300057ce9ac58b
amiltonbhz <71555277+amiltonbhz@users.noreply.github.com>
2023-01-04 09:19:16 -0500

    FInished

--- Web Development Bootcamp/Drum Kit/index.js
+++ Web Development Bootcamp/Drum Kit/index.js
@@ -1,11 +1,68 @@
-//count buttons
-var buttonCount = document.querySelectorAll(".drum").length;
 
-//add on click event listener to buttons
-for (var i=0; i<buttonCount; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click",handleClick);
+// DETECTING MOUSE CLICK
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0 ;i<numberOfDrumButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+    
+        var buttonSound = this.textContent;
+        
+        makeSound(buttonSound);
+
+
+    });
 }
 
-function handleClick(){
-    alert("I got clicked!");
+
+//DETECTING KEYBOARD PRESS
+
+document.addEventListener("keydown", function(event){
+    
+    makeSound(event.key);
+
+})
+
+function makeSound(key){
+
+    switch (key){
+
+        case 'w':
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+         case 'a':
+            var tom2 = new Audio ("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        
+        case 's':
+            var tom3 = new Audio ("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case 'd':
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        
+        case 'j':
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case 'k':
+            var cash = new Audio("sounds/crash.mp3");
+            cash.play();
+            break;
+        
+        case 'l':
+            var kick = new Audio("sounds/kick.mp3");
+            kick.play();
+            break;
+        
+    default: console.log(this);
+
+    }
 }
